c2f2a769cc79838483c61f52a64364a4bf1b9956
mehdi <mehdi@cherti.name>
2015-10-05 14:48:16 +0200

    first commit

---
+++ notebooks/utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# Adapted from : http://scikit-learn.org/stable/auto_examples/semi_supervised/plot_label_propagation_versus_svm_iris.html
+def plot_decision_boundary(clf, X, y, h = .02):
+    
+    # create a mesh to plot in
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    color_map = {-1: (1, 1, 1), 0: (0, 0, .9), 1: (1, 0, 0), 2: (.8, .6, 0)}
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+    plt.axis('off')
+
+    # Plot also the training points
+    colors = [color_map[y] for y in y]
+    plt.scatter(X[:, 0], X[:, 1], c=colors, cmap=plt.cm.Paired)
+    plt.show()
